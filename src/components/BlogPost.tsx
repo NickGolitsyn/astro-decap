d62aa4cae8461e6155be59496a90e7a90184b129
Nick Golitsyn <45421041+NickGolitsyn@users.noreply.github.com>
2023-09-20 11:39:52 +0100

    check

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,114 +1,107 @@
 import { motion } from "framer-motion";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface Props {
   title: string;
   author: string;
-  authorURL: string;
   publishDate: string;
   heroImage: string;
   coverImage: string;
   alt: string;
+	htmlfile: string;
 }
 
-function Article({ title, author, authorURL, publishDate, heroImage, coverImage, alt }: Props) {
+function Article({ title, author, publishDate, heroImage, coverImage, htmlfile, alt }: Props) {
 	const constraintsRef = useRef(null)
-  function getRussianMonth(englishMonth: string) {
-    const months = {
-      Jan: "Янв",
-      Feb: "Фев",
-      Mar: "Мар",
-      Apr: "Апр",
-      May: "Май",
-      Jun: "Июн",
-      Jul: "Июл",
-      Aug: "Авг",
-      Sep: "Сен",
-      Oct: "Окт",
-      Nov: "Ноя",
-      Dec: "Дек"
+  
+
+  const [iframeHeight, setIframeHeight] = useState<number | null>(null);
+
+  useEffect(() => {
+    // Define the resizeIframe function
+    function resizeIframe() {
+      const iframe = document.getElementById('frame') as HTMLIFrameElement;
+
+      if (iframe) {
+        const elementsInsideIframe = iframe.contentDocument?.querySelectorAll('body');
+
+        if (elementsInsideIframe) {
+          elementsInsideIframe.forEach((element) => {
+            element.style.padding = '0';
+            element.style.maxWidth = '100%';
+          });
+
+          const newHeight = iframe.contentWindow.document.documentElement.scrollHeight;
+          if (newHeight !== iframeHeight) {
+            iframe.style.height = newHeight + 'px';
+            setIframeHeight(newHeight);
+          }
+        }
+      }
+    }
+
+    // Attach the resizeIframe function to the iframe's load event
+    const iframe = document.getElementById('frame') as HTMLIFrameElement;
+    if (iframe) {
+      iframe.addEventListener('load', resizeIframe);
+    }
+
+    // Attach the resizeIframe function to the window's resize event
+    window.addEventListener('resize', resizeIframe);
+
+    // Clean up the event listeners when the component unmounts
+    return () => {
+      if (iframe) {
+        iframe.removeEventListener('load', resizeIframe);
+      }
+      window.removeEventListener('resize', resizeIframe);
     };
-
-    return months[englishMonth];
-  }
-
-  const parts = publishDate.split(" ");
-  const day = parts[0];
-  const month = getRussianMonth(parts[1]);
-  const year = parts[2];
-
-  const russianDate = `${day} ${month} ${year}`;
+  }, [iframeHeight]);
 
   return (
     <motion.div className="layout container select-none" ref={constraintsRef}>
-			<style>{`
-	.hero-image {
-		width: 70vw;
-		aspect-ratio: 2/1;
-		object-fit: cover;
-		object-position: center;
-		margin-top: 2rem;
-		margin-bottom: 4rem;
-		max-width: 1280px;
-	}
-
-	@media (max-width: 50em) {
-		.hero-image {
-			height: 260px;
-			margin-top: 0;
-			margin-bottom: 2rem;
-		}
-	}
-
-	.content {
-		margin-bottom: 8rem;
-	}
-
-	.content :global(main > * + *) {
-		margin-top: 1rem;
-	}
-
-	.content :global(h2) {
-		margin-top: 4rem;
-	}
-
-	header {
-		display: flex;
-		flex-direction: column;
-		text-align: center;
-		align-items: center;
-		justify-content: center;
-
-		padding-bottom: 2rem;
-		margin-bottom: 2rem;
-		border-bottom: 4px solid var(--theme-divider);
-	}
-
-	.title,
-	.author,
-	.publish-date {
-		margin: 0;
-	}
-
-	.publish-date,
-	.author {
-		color: var(--theme-text-lighter);
-	}
-
-	.title {
-		font-size: 2.25rem;
-		font-weight: 700;
-	}`}
-</style>
+			<style>
+				{`
+				.hero-image {
+					width: 35vw;
+					object-fit: cover;
+					object-position: center;
+					max-width: 1280px;
+				}
+			
+				.content :global(main > * + *) {
+					margin-top: 1rem;
+				}
+			
+				.content :global(h2) {
+					margin-top: 4rem;
+				}
+			
+				header {
+					display: flex;
+					flex-direction: column;
+					text-align: center;
+					align-items: center;
+					justify-content: center;
+				}
+			
+				.title {
+					font-size: 2.25rem;
+					font-weight: 700;
+					margin: 0;
+				}
+				`}
+			</style>
       <article className="content">
         <div>
-          <header>
-            {coverImage && <motion.img drag dragConstraints={constraintsRef} width="720" height="420" className="hero-image element" loading="lazy" src={coverImage} alt={alt} />}
-            <motion.p drag dragConstraints={constraintsRef} className="publish-date element">{russianDate}</motion.p>
-            <motion.h1 drag dragConstraints={constraintsRef} className="title element">{title}</motion.h1>
-          </header>
+					<header>
+						{coverImage && <img width="720" height="420" className="hero-image element my-5" loading="lazy" src={coverImage} alt={alt} />}
+						<h1 className="title element">{title}</h1>
+						<p>{author}</p>
+					</header>
           <main className="element">
-            <slot />
+						<iframe src={htmlfile} className="w-full" scrolling="no" id="frame" />
+						{/* <embed src={htmlfile} type="application/pdf" width="100%" height="500" /> */}
           </main>
         </div>
       </article>
